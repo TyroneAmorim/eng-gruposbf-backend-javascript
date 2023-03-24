c01b72c91c771088afc57a4cdf3a2781f599af47
Tyrone Amorim <tyrone@trustdeveloper.com.br>
2023-03-24 15:11:06 -0300

    fix: rota ajustada em CurrencyController

--- src/Currency/currency.controller.ts
+++ src/Currency/currency.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 import { CurrencyService } from './currency.service';
 import { Currency } from '../interfaces';
 
-@Controller()
+@Controller('currency')
 export class CurrencyController {
   constructor(private readonly currencyService: CurrencyService) {}
 
-  @Get('/convertion')
+  @Get('convertion')
   getCurrency(): Promise<Currency[]> {
     return this.currencyService.getCurrencys();
   }
