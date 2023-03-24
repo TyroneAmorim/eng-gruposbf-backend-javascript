76bcfe125a7f141f75ee684750e047834be1d7a4
Tyrone Amorim <tyrone@trustdeveloper.com.br>
2023-03-24 15:07:08 -0300

    fix: recurso app ajustado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CurrencyController } from './Currency/currency.controller';
+import { CurrencyService } from './Currency/currency.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [CurrencyController],
+  providers: [CurrencyService],
 })
 export class AppModule {}
