904ea377d2aa60c732d10537fc94a143ae3fb6cd
Tyrone Amorim <tyrone@trustdeveloper.com.br>
2023-03-25 17:27:32 -0300

    fix: ajuste no objeto de retorno da api

--- src/Currency/APIs/AwesomeApi/AwesomeApi.ts
+++ src/Currency/APIs/AwesomeApi/AwesomeApi.ts
@@ -15,14 +15,20 @@ export default class AwesomeApi implements Conversion {
   async getCurrencys() {
     try {
       const currencySearch = this.currencySearch
+        .filter((item) => item !== this.currencyBase)
         .map((item) => `${item}-${this.currencyBase}`)
         .join();
 
-      const { data } = await axios.get(`${this.urlApi}/${currencySearch}`);
+      const resultData = await axios.get(`${this.urlApi}/${currencySearch}`);
+      const data = resultData.data.length
+        ? resultData.data
+        : [{ ...resultData.data[Object.keys(resultData.data)[0]] }];
+
       const result = data.map((item: CurrencyData) => ({
         currency: item.code,
         value: item.ask,
       }));
+
       return result;
     } catch (error) {
       const { message, status } = error.response.data;
