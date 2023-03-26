7862e538e3fe27fae940e92de662127f411378d2
Tyrone Amorim <tyrone@trustdeveloper.com.br>
2023-03-26 00:30:30 -0300

    refactor: funcao para validar moeda refatorada

--- src/utils/CurrencyByLocation.ts
+++
@@ -1,8 +0,0 @@
-import { CurrencyTool } from 'src/interfaces';
-import { getCurrency as getFromLocale } from 'locale-currency';
-
-export default class implements CurrencyTool {
-  getCurrency(lang: string): string {
-    return getFromLocale(lang);
-  }
-}
