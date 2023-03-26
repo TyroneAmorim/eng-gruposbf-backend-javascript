57f35d13620b358355372e170df58c93fee57ef2
Tyrone Amorim <tyrone@trustdeveloper.com.br>
2023-03-26 00:32:52 -0300

    chore: migration atualizada

--- db/migrations/1679723172779-PopulateParams.ts
+++ db/migrations/1679723172779-PopulateParams.ts
@@ -5,21 +5,13 @@ export class PopulateParams1679723172779 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.manager.save(CurrencyEntity, [
       {
-        nome: 'moedaBase',
-        valor: 'BRL',
-      },
-      {
         nome: 'moedasDisponiveis',
-        valor: ['USD', 'EUR'],
+        valor: ['BRL', 'USD', 'EUR', 'INR'],
       },
     ]);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.manager.delete(CurrencyEntity, {
-      nome: 'moedaBase',
-    });
-
     await queryRunner.manager.delete(CurrencyEntity, {
       nome: 'moedasDisponiveis',
     });
