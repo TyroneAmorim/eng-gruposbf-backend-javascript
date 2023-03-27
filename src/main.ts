b30807b172db781ba865e453909f47cddf0649a9
Tyrone Amorim <tyrone@trustdeveloper.com.br>
2023-03-27 17:48:40 -0300

    fix: validacao global adicionada

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(env.PORT);
 }
 bootstrap();
